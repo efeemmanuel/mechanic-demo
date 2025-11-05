ad11ac7b6248009a669156094f3efe7a84750e62
Efe Emmanuel <efeemmanuel2030@gmail.com>
2025-11-05 07:58:26 +0100

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -178,7 +178,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://asoro-backend.onrender.com/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
